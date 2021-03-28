c0a1cb1f6bb919998ae8a63b321f1f271b9fb77a
Md. Masudul Kabir <masud.ncse@gmail.com>
2021-03-28 16:22:29 +0600

    add dashboard

---
+++ src/views/Dashboard/Layout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="dashboard">
+    <a-layout id="components-layout-demo-top-side-2">
+      <TheHeader />
+      <a-layout>
+        <TheSidebar />
+
+        <a-layout style="padding: 0 24px 24px">
+          <router-view></router-view>
+          <TheFooter />
+        </a-layout>
+      </a-layout>
+    </a-layout>
+  </div>
+</template>
+<script>
+import TheHeader from "../../components/TheHeader";
+import TheSidebar from "../../components/TheSidebar";
+import TheFooter from "../../components/TheFooter";
+
+export default {
+  components: {
+    TheHeader,
+    TheSidebar,
+    TheFooter,
+  },
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
+};
+</script>
+<style>
+#components-layout-demo-top-side-2 .logo {
+  width: 120px;
+  height: 31px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px 28px 16px 0;
+  float: left;
+}
+</style>
